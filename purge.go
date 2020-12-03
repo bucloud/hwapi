dc92c1ec7adbb79b9a25f3aaef85760f2e0d42da
MagicQ <magicq@buwebcloud.com>
2020-12-03 23:15:04 +0800

    update purge method

--- purge.go
+++ purge.go
@@ -4,29 +4,46 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"net/http"
+	"net/url"
 )
 
+// Purges used by post body
 type Purges struct {
 	List []*Purge `json:"list"`
 }
 
+// WarmUp alias Purge
+type WarmUp struct {
+	URL       string   `json:"url"`
+	Recursive bool     `json:"recursive"`
+	Range     bool     `json:"-"`
+	FixRange  string   `json:"-"`
+	Headers   []string `json:"-"`
+
+	// RegionFilter control which pop handle warmup
+	RegionFilter string `json:"-"`
+}
+
+// Purge object
 type Purge struct {
-	Url             string `json:"url"`
+	URL             string `json:"url"`
 	Recursive       bool   `json:"recursive"`
 	PurgeAllDynamic bool   `json:"purgeAllDynamic"`
 }
 
+// PurgeState purge response
 type PurgeState struct {
-	Id       string  `json:"id"`
+	ID       string  `json:"id"`
 	Progress float32 `json:"progress"`
 }
 
-//Get purge status
+// GetPurgeState check purge state by purgeID
 func (api *HWApi) GetPurgeState(accountHash string, purgeID string) (float32, error) {
 	r, e := api.Request(
 		&Request{
 			Method: GET,
-			Url:    fmt.Sprintf("/api/v1/accounts/%s/purge/%s", accountHash, purgeID),
+			URL:    fmt.Sprintf("/api/v1/accounts/%s/purge/%s", accountHash, purgeID),
 		},
 	)
 	if e != nil {
@@ -36,24 +53,25 @@ func (api *HWApi) GetPurgeState(accountHash string, purgeID string) (float32, er
 	return al.Progress, json.Unmarshal(r.body, al)
 }
 
-func parsePugeList(p []interface{}) (*Purges, error) {
+func parsePurgeList(p []interface{}) (*Purges, error) {
 	pl := &Purges{}
 	if len(p) == 0 {
 		return nil, errors.New("purgeList nil exception")
-	} else {
-		for _, v := range p {
-			switch x := v.(type) {
-			case string:
-				pl.List = append(pl.List, &Purge{
-					Url: x,
-				})
-			case *Purge:
-				pl.List = append(pl.List, x)
-			default:
-				return nil, errors.New("Only string/Purge are supported")
-			}
+	}
+
+	for _, v := range p {
+		switch x := v.(type) {
+		case string:
+			pl.List = append(pl.List, &Purge{
+				URL: x,
+			})
+		case *Purge:
+			pl.List = append(pl.List, x)
+		default:
+			return nil, errors.New("Only string/*Purge are supported")
 		}
 	}
+
 	return pl, nil
 }
 
@@ -61,14 +79,14 @@ func parsePugeList(p []interface{}) (*Purges, error) {
 //Usage Purge("a1b1c1d1",Purge{url:"1"},Purge{url:"2"}) or Purge("a1b1c1d1",[Purge{url:"1"},Purge{url:"2"}])
 //You can Purge{url:"1"} as "1"
 func (api *HWApi) Purge(accountHash string, purgeList ...interface{}) (*PurgeState, error) {
-	pl, err := parsePugeList(purgeList)
+	pl, err := parsePurgeList(purgeList)
 	if err != nil {
 		return nil, err
 	}
 	r, e := api.Request(
 		&Request{
 			Method: GET,
-			Url:    fmt.Sprintf("/api/v1/accounts/%s/purge", accountHash),
+			URL:    fmt.Sprintf("/api/v1/accounts/%s/purge", accountHash),
 			Body:   pl,
 		},
 	)
@@ -78,25 +96,66 @@ func (api *HWApi) Purge(accountHash string, purgeList ...interface{}) (*PurgeSta
 	al := &PurgeState{}
 	return al, json.Unmarshal(r.body, al)
 }
+func warmUpConvert(i interface{}) *WarmUp {
+	switch i.(type) {
+	case string:
+		return &WarmUp{
+			URL: i.(string),
+		}
+	case *WarmUp:
+		return i.(*WarmUp)
+	default:
+		return &WarmUp{
+			URL: "http://205.185.216.10/",
+		}
+	}
+}
 
-//Prewarm Url
-//Usage Purge("a1b1c1d1",Purge{url:"1"},Purge{url:"2"}) or Purge("a1b1c1d1",[Purge{url:"1"},Purge{url:"2"}])
-//You can Purge{url:"1"} as "1"
-func (api *HWApi) WarmUpUrl(accountHash string, purgeList ...interface{}) (*PurgeState, error) {
-	pl, err := parsePugeList(purgeList)
-	if err != nil {
-		return nil, err
+//WarmUp Url
+//Usage WarmUp("a1b1c1d1",WarmUp{url:"1"},WarmUp{url:"2"}) or WarmUp("a1b1c1d1",[WarmUp{url:"1"},WarmUp{url:"2"}])
+func (api *HWApi) WarmUp(accountHash string, warmList ...interface{}) (bool, error) {
+	if len(warmList) == 0 {
+		return false, fmt.Errorf("nothing todo")
 	}
-	r, e := api.Request(
-		&Request{
-			Method: GET,
-			Url:    fmt.Sprintf("/api/v1/accounts/%s/purge", accountHash),
-			Body:   pl,
-		},
-	)
+	// get pop list
+	pops, e := api.GetPoPs()
 	if e != nil {
-		return nil, e
+		return false, e
 	}
-	al := &PurgeState{}
-	return al, json.Unmarshal(r.body, al)
+	// Get test url
+	var tu *url.URL
+	w0 := warmUpConvert(warmList[0])
+	tu, _ = url.Parse(w0.URL)
+	// check available pops
+	availablePOP := []*POP{}
+	for _, pop := range pops.List {
+		tu.Host = "doppler." + pop.Code + ".hwcdn.net"
+		_, e := api.hc.RoundTrip(&http.Request{
+			Method: GET,
+			URL:    tu,
+		})
+		if e != nil {
+			// unknow error
+			fmt.Printf("unknow error %s teat as failed", e.Error())
+		}
+		availablePOP = append(availablePOP, pop)
+	}
+	// for _, u := range warmList {
+	// 	var tempURL *url.URL
+	// 	wt := warmUpConvert(u)
+	// 	tempURL, _ = url.Parse(wt.URL)
+	// 	for _, p := range availablePOP {
+	// 		tempURL.Host = "doppler." + p.Code + ".hwcdn.net"
+
+	// 		_, e := api.hc.RoundTrip(&http.Request{
+	// 			URL:    tempURL,
+	// 			Method: "GET",
+	// 			Header: http.Header{
+	// 				"Host":""
+	// 			},
+	// 		})
+	// 	}
+	// }
+
+	return true, nil
 }
