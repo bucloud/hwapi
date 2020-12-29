6a63da50342b15c525795591c3920d679ef62d26
MagicQ-BuCloud <44394665+MagicQ-BuCloud@users.noreply.github.com>
2020-12-29 20:15:07 +0800

    Update hwapi.go

--- hwapi.go
+++ hwapi.go
@@ -84,7 +84,7 @@ func Init(options ...interface{}) *HWApi {
 		}
 	}
 	if api.cache == nil {
-		api.cache, _ = fastcache.LoadFromFileOrNew(cacheFilePath,maxCacheSize)
+		api.cache = fastcache.LoadFromFileOrNew(cacheFilePath,maxCacheSize)
 	}
 	return api
 }
